093df516f3e3b60a991152a2e283bf4ed380cb71
Nikita Stasiulevich <nikitastas1694@gmail.com>
2021-03-07 23:27:10 +0300

    solve tast

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array == 0 || array == undefined) {
+    return 0;
+  } else {
+    return Math.min(...array);
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == 0 || array == undefined) {
+    return 0;
+  } else {
+    return Math.max(...array);
+  } 
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == 0 || array == undefined) {
+    return 0;
+  } else {
+
+    let sum = 0;
+
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i]; 
+    }
+    return sum / array.length;
+  }
 }
